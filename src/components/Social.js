cd2ff725369e144e6ae511fc23318b2985bf062c
Gahie Okou <gokou097@gmail.com>
2022-10-31 02:37:46 +0000

    Open socials in new link

--- src/components/Social.js
+++ src/components/Social.js
@@ -6,9 +6,9 @@ const Social = () => {
         <div className="social">
             <h2>Socials</h2>
 
-            <a href="mailto:gokou097@gmail.com?subject=I%20saw%20your%20portfolio" className="button"><AiFillMail/> gokou097@gmail.com</a>
-            <a href="https://www.linkedin.com/in/gokou/" className="button"><AiFillLinkedin /> LinkedIn</a>
-            <a href="https://github.com/WSGokou/" className="button"><AiFillGithub /> GitHub</a>
+            <a href="mailto:gokou097@gmail.com?subject=I%20saw%20your%20portfolio" target="_blank" className="button"><AiFillMail/> gokou097@gmail.com</a>
+            <a href="https://www.linkedin.com/in/gokou/" target="_blank" className="button"><AiFillLinkedin /> LinkedIn</a>
+            <a href="https://github.com/WSGokou/" target="_blank" className="button"><AiFillGithub /> GitHub</a>
         </div>
     );
 }
