dc62eacb496b87f1b0aa65a1e05b2bf9608a3d40
Gahie Okou <gokou097@gmail.com>
2022-11-09 03:56:18 +0000

    Todo project added

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,5 +1,6 @@
 // Import Assets
 import rng from '../assets/rng.png';
+import todo from '../assets/todo.png'
 
 const Projects = () => {
     return (
@@ -12,11 +13,22 @@ const Projects = () => {
                     <h3>Indecision App</h3>
                     <img src={rng} alt="Random Option Picker" />
                     <p>Random Option Picker application built using React, webpack, and sass.
-                        Hosted using netlify app
+                        Hosted using netlify app 
+                    </p>
+                    <br />
+
+                    <a href="https://gokou-tstodo.on.fleek.co" target="_blank" className="button">Site</a>
+                    <a href="https://github.com/WSGokou/Indecision-app" target="_blank" className="button">Code</a>
+                </div>
+                <div className="projects__card">
+                    <h3>Todo App</h3>
+                    <img src={todo} alt="Todo App" />
+                    <p>Todo list application built using Typescript, making use of type system and DOM interaction to add todo's to list
+                        Hosted on the IPFS using fleek
                     </p>
 
                     <a href="https://rngeez.netlify.app" target="_blank" className="button">Site</a>
-                    <a href="https://github.com/WSGokou/Indecision-app" target="_blank" className="button">Code</a>
+                    <a href="https://github.com/WSGokou/typescript-todo" target="_blank" className="button">Code</a>
                 </div>
             </div>
         </section>
